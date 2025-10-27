ba304af1a55f3a60fad42ac301b5fc7cd81efa9b
VanshBatas <vbat0005@student.monash.edu>
2025-10-27 23:39:52 +1100

    Completed Data Visualisation 2 Assignment

---
+++ js/vega_embed.js
@@ -0,0 +1,3 @@
+vegaEmbed("#arrivals_line", "charts/arrivals_line.json");
+vegaEmbed("#symbol_map", "charts/symbol_map.json");
+vegaEmbed("#visitors_expenditure_bar", "charts/visitors_expenditure_bar.json");
